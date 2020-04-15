34c5701e3ab0c91645bd5886266b8e518d2cfbe5
Phillip Nguyen <nh273@cornell.edu>
2020-04-15 18:09:47 +0200

    modifying tones to be more casual

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,9 +4,7 @@ class About extends Component {
   render() {
     return (
       <div className="about">
-        <h1>
-          Edit About component or pages/about.jsx to include your information.
-        </h1>
+        <h1>Những người đóng góp:</h1>
       </div>
     )
   }
